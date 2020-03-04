c29f107eb03611379caddce88e55702dba1cfcd4
Abel Joseph <abeljohnjoseph@gmail.com>
2020-03-04 09:33:32 -0500

    Add musculoskeletal.py

---
+++ musculoskeletal.py
@@ -0,0 +1,267 @@
+import collections
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+from scipy.optimize import fsolve
+from sklearn import linear_model, preprocessing
+from scipy.special import expit
+
+
+class HillTypeMuscle:
+    """
+    Damped Hill-type muscle model adapted from Millard et al. (2013). The
+    dynamic model is defined in terms of normalized length and velocity.
+    To model a particular muscle, scale factors are needed for force, CE
+    length, and SE length. These are given as constructor arguments.
+    """
+
+    def __init__(self, f0M, resting_length_muscle, resting_length_tendon):
+        """
+        :param f0M: maximum isometric force
+        :param resting_length_muscle: actual length (m) of muscle (CE) that corresponds to
+            normalized length of 1
+        :param resting_length_tendon: actual length of tendon (m) that corresponds to
+            normalized length of 1
+        """
+        self.f0M = f0M
+        self.resting_length_muscle = resting_length_muscle
+        self.resting_length_tendon = resting_length_tendon
+
+    def norm_tendon_length(self, muscle_tendon_length, normalized_muscle_length):
+        """
+        :param muscle_tendon_length: non-normalized length of the full muscle-tendon
+            complex (typically found from joint angles and musculoskeletal geometry)
+        :param normalized_muscle_length: normalized length of the contractile element
+            (the state variable of the muscle model)
+        :return: normalized length of the tendon
+        """
+        return (muscle_tendon_length - self.resting_length_muscle * normalized_muscle_length) / self.resting_length_tendon
+
+    def get_force(self, total_length, norm_muscle_length):
+        """
+        :param total_length: muscle-tendon length (m)
+        :param norm_muscle_length: normalized length of muscle (the state variable)
+        :return: muscle tension (N)
+        """
+        return self.f0M * force_length_tendon(self.norm_tendon_length(total_length, norm_muscle_length))
+
+
+def get_velocity(a, lm, lt):
+    """
+    :param a: activation (between 0 and 1)
+    :param lm: normalized length of muscle (contractile element)
+    :param lt: normalized length of tendon (series elastic element)
+    :return: normalized lengthening velocity of muscle (contractile element)
+    """
+    beta = 0.1 # damping coefficient (see damped model in Millard et al.)
+
+    # WRITE CODE HERE TO CALCULATE VELOCITY
+    
+
+
+def force_length_tendon(lt):
+    """
+    :param lt: normalized length of tendon (series elastic element)
+    :return: normalized tension produced by tendon
+    """
+    lts = 1
+    rt = []
+    for length in lt:
+        if length >= lts:
+            rt.append(10*(length - lts) + 240*((length - lts)**2))
+        else: 
+            rt.append(0)
+    
+    return rt / abs(max(min(rt), max(rt), key=abs))
+
+
+def force_length_parallel(lm):
+    """
+    :param lm: normalized length of muscle (contractile element)
+    :return: normalized force produced by parallel elastic element
+    """
+    lpes = 1
+    rt = []
+    for length in lm:
+        if length >= lpes:
+            rt.append(3*((length - lpes)**2) / (.6 + length - lpes))
+        else: 
+            rt.append(0)
+       
+    return rt / abs(max(min(rt), max(rt), key=abs))
+
+
+def plot_curves():
+    """
+    Plot force-length, force-velocity, SE, and PE curves.
+    """
+    lm = np.arange(0, 1.8, .01)
+    vm = np.arange(-1.2, 1.2, .01)
+    lt = np.arange(0, 1.07, .01)
+    plt.subplot(2,1,1)
+    plt.plot(lm, force_length_muscle(lm), 'r')
+    plt.plot(lm, force_length_parallel(lm), 'g')
+    plt.plot(lt, force_length_tendon(lt), 'b')
+    plt.legend(('CE', 'PE', 'SE'))
+    plt.xlabel('Normalized length')
+    plt.ylabel('Force scale factor')
+    plt.subplot(2, 1, 2)
+    plt.plot(vm, force_velocity_muscle(vm), 'k')
+    plt.xlabel('Normalized muscle velocity')
+    plt.ylabel('Force scale factor')
+    plt.tight_layout()
+    plt.show()
+
+
+class Gaussian:
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+
+    def __call__(self, x):
+        return np.exp(-(x-self.mu)**2/2/self.sigma**2)
+
+
+class Sigmoid:
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+
+    def __call__(self, x):
+        return expit((x-self.mu) / self.sigma)
+
+
+class Regression():
+    """
+    1D regression model with Gaussian basis functions.
+    """
+
+    def __init__(self, x, t, centres, width, regularization_weight=1e-6, sigmoids=False):
+        """
+        :param x: samples of an independent variable
+        :param t: corresponding samples of a dependent variable
+        :param centres: a vector of Gaussian centres (should have similar range of values as x)
+        :param width: sigma parameter of Gaussians
+        :param regularization_weight: regularization strength parameter
+        """
+        if sigmoids:
+            self.basis_functions = [Sigmoid(centre, width) for centre in centres]
+        else:
+            self.basis_functions = [Gaussian(centre, width) for centre in centres]
+        self.ridge = linear_model.Ridge(alpha=regularization_weight, fit_intercept=False)
+        self.ridge.fit(self._get_features(x), t)
+
+    def eval(self, x):
+        """
+        :param x: a new (or multiple samples) of the independent variable
+        :return: the value of the curve at x
+        """
+        return self.ridge.predict(self._get_features(x))
+
+    def _get_features(self, x):
+        if not isinstance(x, collections.Sized):
+            x = [x]
+
+        phi = np.zeros((len(x), len(self.basis_functions)))
+        for i, basis_function in enumerate(self.basis_functions):
+            phi[:,i] = basis_function(x)
+        return phi
+
+
+def get_muscle_force_velocity_regression():
+    data = np.array([
+        [-1.0028395556708567, 0.0024834319945283845],
+        [-0.8858611825192801, 0.03218792009622429],
+        [-0.5176245843258415, 0.15771090304473967],
+        [-0.5232565269687035, 0.16930496922242444],
+        [-0.29749770052593094, 0.2899790099290114],
+        [-0.2828848376217543, 0.3545364496120378],
+        [-0.1801231103040022, 0.3892195938775034],
+        [-0.08494610976156225, 0.5927831890757294],
+        [-0.10185137142991896, 0.6259097662790973],
+        [-0.0326643239546236, 0.7682365981934388],
+        [-0.020787245583830716, 0.8526638522676352],
+        [0.0028442725407418212, 0.9999952831301149],
+        [0.014617579774061973, 1.0662107025777694],
+        [0.04058866536166583, 1.124136223202283],
+        [0.026390887007381902, 1.132426122025424],
+        [0.021070257776939272, 1.1986556920827338],
+        [0.05844673474682183, 1.2582274002971627],
+        [0.09900238201929201, 1.3757434966156459],
+        [0.1020023112662436, 1.4022310794556732],
+        [0.10055894908138963, 1.1489210160137733],
+        [0.1946227683309354, 1.1571212943090965],
+        [0.3313459588217258, 1.152041225442796],
+        [0.5510200231126625, 1.204839508502158]
+    ])
+
+    velocity = data[:,0]
+    force = data[:,1]
+
+    centres = np.arange(-1, 0, .2)
+    width = .15
+    result = Regression(velocity, force, centres, width, .1, sigmoids=True)
+
+    return result
+
+
+def get_muscle_force_length_regression():
+    """
+    CE force-length data samples from Winters et al. (2011) Figure 3C,
+    normalized so that max force is ~1 and length at max force is ~1.
+    The sampples were taken form the paper with WebPlotDigitizer, and
+    cut-and-pasted here.
+
+    WRITE CODE HERE 1) Use WebPlotDigitizer to extract force-length points
+    from Winters et al. (2011) Figure 3C, which is on Learn. Click
+    "View Data", select all, cut, and paste below. 2) Normalize the data
+    so optimal length = 1 and peak = 1. 3) Return a Regression object that
+    uses Gaussian basis functions. 
+    """
+    with open('TA_active.csv') as csvfile:
+        d = []
+        reader = csv.reader(csvfile)
+        for row in reader:
+            d.append([float(row[0]), float(row[1])])
+        
+        data = np.array(d)
+
+    peak_force = max(data[:,1])  # peak of the force-length graph
+    optimal_length = float()
+
+    for i, pt in enumerate(data):
+        if pt[1] == peak_force:
+            optimal_length = pt[0]
+
+    length = data[:,0] / optimal_length
+    force = data[:,1] / peak_force
+
+    centres = np.arange(min(length), max(length), .2)
+    width = .15
+    result = Regression(length, force, centres, width, regularization_weight=.1, sigmoids=False)
+
+    return result
+
+
+
+force_length_regression = get_muscle_force_length_regression()
+force_velocity_regression = get_muscle_force_velocity_regression()
+
+
+def force_length_muscle(lm):
+    """
+    :param lm: muscle (contracile element) length
+    :return: force-length scale factor
+    """
+    return force_length_regression.eval(lm)
+
+
+def force_velocity_muscle(vm):
+    """
+    :param vm: muscle (contractile element) velocity)
+    :return: force-velocity scale factor
+    """
+    return np.maximum(0, force_velocity_regression.eval(vm))
+
+
+plot_curves()
